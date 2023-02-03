274baa11a2d6c980de3cc34634dec1abffe46903
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-02-03 11:09:05 -0500

    s2c2 flex menu et changement de couleur

--- header.php
+++ header.php
@@ -8,9 +8,9 @@
 </head>
 <body>
     <header>
+        <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
+        <h2><?= bloginfo('description') ?></h2>
         <?php wp_nav_menu(array(
             "menu"=> "entete"
         )) ?>
-        <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
-        <h2><?= bloginfo('description') ?></h2>
     </header>
