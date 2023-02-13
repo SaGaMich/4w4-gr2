6d0c201e653f59080104bffa0f92dba8534a69f6
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-02-13 13:55:54 -0500

    s4c1 markdown et bloc flex

--- front-page.php
+++ front-page.php
@@ -7,22 +7,23 @@
 <?php get_header(); ?>
 <main>
     <h1>Bienvenue au cours de 4w4</h1>
-    <?php
-        if(have_posts()):
-            while (have_posts()) : the_post(); 
-    ?>
-        <h1>
-            <a href="<?php echo get_permalink();?>"> <?php echo get_the_title(); ?> </a>
-        </h1>
-
-    <?php //the_content(); // affiche le contenu complet de l'article?>
-    <?php //the_excerpt(); // affiche une résumé de l'article ?> 
-    <?= wp_trim_words(get_the_excerpt(), 10, "...") ?>  
-
-    <hr>
-    <?php 
-        endwhile;
-        endif;
-    ?>
+    <section class="blocflex">
+        <?php
+            if(have_posts()):
+                while (have_posts()) : the_post(); 
+        ?>
+        <article>
+        <a href="<?php echo get_permalink();?>">
+            <h2><?php echo get_the_title(); ?></h2>
+            <?php //the_content(); // affiche le contenu complet de l'article?>
+            <?php //the_excerpt(); // affiche une résumé de l'article ?> 
+            <p><?= wp_trim_words(get_the_excerpt(), 10, "...") ?></p>
+        </a>
+        </article>
+        <?php 
+            endwhile;
+            endif;
+        ?>
+    </section>
 </main>
 <?php get_footer(); ?>
