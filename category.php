cf5219c07fb2a1417db10f4839a29c21ec49f1f3
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-02-24 09:06:02 -0500

    s5c2 tri des catégories

--- category.php
+++ category.php
@@ -4,24 +4,27 @@
 *
 */
 ?>
-<?php get_header(); ?>
-<main>
-<h6><?php echo category_description(); ?></h6>
-    <section class="blocflex">
-        <?php
-            if(have_posts()):
-                while (have_posts()) : the_post(); 
-        ?>
-        <article>
-        <a href="<?php echo get_permalink();?>">
-            <h3><?php echo get_the_title(); ?></h3>
-            <p><?= wp_trim_words(get_the_excerpt(), 10, "...") ?></p>
-        </a>
-        </article>
-        <?php 
-            endwhile;
-            endif;
-        ?>
-    </section>
+<main class="site__main">
+    <code>category.php</code>
+   <section class="blocflex">
+      <?php
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
+            <article>
+               <h2><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h2>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
 </main>
+
 <?php get_footer(); ?>
