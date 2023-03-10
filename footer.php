4384ccba0ede72a540d5863f0f1fdbeeb4c8c685
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-03-10 11:01:07 -0500

    s7c2 footer

--- footer.php
+++ footer.php
@@ -1,9 +1,21 @@
 <footer class="site_footer">
     <section>
-        <p>logo</p>
-        <p>lien</p>
-        <p>réseaux sociaux</p>
+            <div class="logo_footer">
+                <?php the_custom_logo(); ?>
+            </div>
     </section>
+    <section>
+        <p>lien</p>
+    </section>
+    <section class="réseaux">
+        <p>réseaux sociaux </p>
+        <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000" width="32" height="32">
+        <img src="https://s2.svgbox.net/social.svg?ic=youtube&color=000" width="32" height="32">
+        <img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32">
+        <img src="https://s2.svgbox.net/social.svg?ic=discord&color=000" width="32" height="32">
+        
+    </section>
+    
 </footer>
 <?php wp_footer(); ?>    
 </body>
