6871130a1ff95ec737cee0b64ade88e465f88a4f
Michelle Salois Gadoury <e2078172@cmaisonneuve.qc.ca>
2023-03-13 12:20:51 -0400

    s8c1 mofification du aside et du footer

--- footer.php
+++ footer.php
@@ -4,16 +4,21 @@
                 <?php the_custom_logo(); ?>
             </div>
     </section>
-    <section>
-        <p>lien</p>
+    <section class="menu_footer">
+        <p>Menu</p>
+        <?php wp_nav_menu(array(
+                    "menu"=> "entete",
+                    "container" => "nav"
+                )) ?>
     </section>
-    <section class="réseaux">
-        <p>réseaux sociaux </p>
+    <section class="reseaux">
+        <p>Réseaux sociaux </p>
+        <div class="logo_reseaux">
         <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000" width="32" height="32">
         <img src="https://s2.svgbox.net/social.svg?ic=youtube&color=000" width="32" height="32">
         <img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32">
         <img src="https://s2.svgbox.net/social.svg?ic=discord&color=000" width="32" height="32">
-        
+        </div>
     </section>
     
 </footer>
--- functions.php
+++ functions.php
@@ -6,7 +6,11 @@ wp_enqueue_style(   'style-principale', //identificateur du link css
 					get_template_directory_uri() . '/style.css', // l'endroit où se trouve le fichier style.css
 					array(), // les fichiers css qui dépendent de styles.css
 					filemtime(get_template_directory() . '/style.css') // la version de style.css
-				);	
+				);
+
+	/*wp_enqueue_style("style-google-font",
+	'https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:wght@300&family=Ubuntu&display=swap',
+	false);*/
 }
 
 add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
@@ -26,7 +30,9 @@ function personalisation_menu_item_title($title, $item, $args, $depth) {
     // Remplacer 'cours' par l'identifiant de votre menu
     if($args->menu == 'cours') {
 // Modifier la longueur du titre en fonction de vos besoins
-$title = wp_trim_words($title, 3, ' ... '); // on garde uniquement trois mots pourle titre du choix
+$sigle = substr($title,0,7);
+$title = substr($title,7);
+$title = "<code>" . $sigle . "</code>" . "<p>" .wp_trim_words($title, 3, ' ... ') . "</p>"; // on garde uniquement trois mots pourle titre du choix
 }
 return $title;
 }
