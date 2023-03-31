35fafda7146789e8f91c431a03ae52be9b0381e2
Michelle <e2078172@cmaisonneuve.qc.ca>
2023-03-31 10:07:56 -0400

    c2s9 création des template-part cours et note-4w4

--- search.php
+++ search.php
@@ -5,8 +5,19 @@
  */
 ?>
 <?php get_header(); ?>
+
 <main class="résultat-recherche">
 <h2>Résultats de la recherche</h2>
+<?php
+      /*$category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+            if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post();*/ ?>
 <?php
     if (have_posts()): 
         while (have_posts()) : the_post();?>  
@@ -20,6 +31,10 @@
     <?php endwhile;
         endif;
     ?>
+         <?php
+         /*$category->slug peut avoir une de ces deux valeur : "cours" ou "note-4w4"*/
+        /* get_template_part('template-parts/search', $category->slug);*/?>
+             
 </main>
 
 <?php get_footer(); ?>
